367a2c38f0ccc9c4b58a9f9c831adb526502cd94
Romashka <GrebenevRV@gmail.com>
2024-10-18 22:45:31 +0300

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import React from "react";
+
+export default function MainPage() {
+  return (
+    <div>
+      <div className="container">Контейнер</div>
+
+      <div>wefwef</div>
+
+      <div>1</div>
+
+      <div>wefwe</div>
+
+      <div>ffwef</div>
+    </div>
+  );
+}
