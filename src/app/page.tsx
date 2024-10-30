ac04f074abc78e6ff3b8fd33f05ab1ffd154b7d0
Romashka <GrebenevRV@gmail.com>
2024-10-30 10:57:11 +0300

    refactoring: setup project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,5 @@
-"use client";
-
 import React from "react";
 
 export default function MainPage() {
-  return (
-    <div>
-      <div className="container">Контейнер</div>
-
-      <div>wefwef</div>
-
-      <div>1</div>
-
-      <div>wefwe</div>
-
-      <div>ffwef</div>
-    </div>
-  );
+  return <div className="container">Контейнер</div>;
 }
